c957ae4318fe3bb97171f759277a979a3909b8d2
Eliandani <220711965@students.uajy.ac.id>
2025-06-08 19:16:39 +0700

    update backend hunter

--- routes/api.php
+++ routes/api.php
@@ -10,18 +10,23 @@ use App\Http\Controllers\OrganisasiController;
 use App\Http\Controllers\AlamatController;
 use App\Http\Controllers\PembeliController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\HunterController;
 use App\Http\Controllers\Api\KategoriApiController;
 use App\Http\Controllers\Api\BarangTitipanController;
 
 Route::post('/login', [LoginController::class, 'loginMobile'])->name('login');
 
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [LoginController::class, 'logoutMobile'])->name('logout');
+    Route::post('/logout', [LoginController::class, 'logoutMobile']);
     Route::get('show', [PembeliController::class, 'showM'])->name('pembeli.show');
     Route::get('show', [PenitipController::class, 'showM'])->name('penitip.show');
     Route::get('show', [PegawaiController::class, 'showM'])->name('pegawai.show');
     Route::get('/kurir-index',[KurirController::class, 'index']);
+    Route::get('/hunter-index',[HunterController::class, 'index']);
     Route::post('/save-fcm-token-pegawai',[PegawaiController::class, 'saveFcmToken']);
+    Route::get('/hunter/komisi/total', [HunterController::class, 'getTotalKomisiHunter']);
+    Route::get('/hunter-history-komisi', [HunterController::class, 'historyKomisi']);
+
 });
 
 Route::middleware(['auth:sanctum'])->group(function () {
