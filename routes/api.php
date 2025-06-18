f141b6777c5d503899dfd2a459dcc42c24cfbe43
Eliandani <220711965@students.uajy.ac.id>
2025-06-18 17:08:15 +0700

    upDATE

--- routes/api.php
+++ routes/api.php
@@ -40,6 +40,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/merchandise', [MerchandiseController::class, 'index']);
     Route::post('/reward/claim-merchandise', [RewardController::class, 'claimMerchandise']);
     Route::get('/reward/history/{id_pembeli}', [RewardController::class, 'history']);
+    Route::get('/hunter-history-komisi-livecode', [HunterController::class, 'historyKomisiLiveCode']);
 
 });
 
